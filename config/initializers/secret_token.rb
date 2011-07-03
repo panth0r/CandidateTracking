662f9abaf1ff7125fcd02f6a112fb82b4118b98c
Kory Woods <kory@virlo.net>
2011-07-03 18:23:25 -0500

    rails bones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CandidateTracking::Application.config.secret_token = 'd8f6358ebcabfc4b488b81b7200b924c688b5941d4d3af03aa0221212aa7bb31a024f11d90b20f408d552ceca3a393457d2a7cda7afcd207507c89ec2de3459f'
