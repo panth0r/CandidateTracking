12928d1f8713d38404fa53dc9bc4c7399baa101a
Michael Kory Woods <kory@virlo.net>
2011-07-10 17:36:50 -0500

    added user authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ CandidateTracking::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  # Added per devise configuration
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110710192158) do
+ActiveRecord::Schema.define(:version => 20110710215036) do
 
   create_table "users", :force => true do |t|
     t.string   "fname"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(:version => 20110710192158) do
     t.text     "cletter"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
