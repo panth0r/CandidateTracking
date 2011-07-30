27e1b05917cf90ecb93083962474ee08192f26a3
Michael Kory Woods <kory@virlo.net>
2011-07-30 15:11:06 -0500

    merging

---
+++ app/controllers/applikations_controller.rb
@@ -0,0 +1,83 @@
+class ApplikationsController < ApplicationController
+  # GET /applikations
+  # GET /applikations.xml
+  def index
+    @applikations = Applikation.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @applikations }
+    end
+  end
+
+  # GET /applikations/1
+  # GET /applikations/1.xml
+  def show
+    @applikation = Applikation.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @applikation }
+    end
+  end
+
+  # GET /applikations/new
+  # GET /applikations/new.xml
+  def new
+    @applikation = Applikation.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @applikation }
+    end
+  end
+
+  # GET /applikations/1/edit
+  def edit
+    @applikation = Applikation.find(params[:id])
+  end
+
+  # POST /applikations
+  # POST /applikations.xml
+  def create
+    @applikation = Applikation.new(params[:applikation])
+
+    respond_to do |format|
+      if @applikation.save
+        format.html { redirect_to(@applikation, :notice => 'Applikation was successfully created.') }
+        format.xml  { render :xml => @applikation, :status => :created, :location => @applikation }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @applikation.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /applikations/1
+  # PUT /applikations/1.xml
+  def update
+    @applikation = Applikation.find(params[:id])
+
+    respond_to do |format|
+      if @applikation.update_attributes(params[:applikation])
+        format.html { redirect_to(@applikation, :notice => 'Applikation was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @applikation.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /applikations/1
+  # DELETE /applikations/1.xml
+  def destroy
+    @applikation = Applikation.find(params[:id])
+    @applikation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(applikations_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
